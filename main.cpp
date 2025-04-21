2aae9f2bbf6a5d433fc74cebad6403fee6cd0ccb
saeid <h.s.bijani@gmail.com>
2025-04-21 17:52:39 +0200

    sum count

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int a = 5;                                       // this is a variable
+    int b = 10;                                      // this is another variable
+    int sum = a + b;                                 // this is a sum
+    std::cout << "The sum is: " << sum << std::endl; // this is a print statement
+
     return 0;
 }
